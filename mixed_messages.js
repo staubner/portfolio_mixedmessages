f8e5400bc41dd10c906d9c3db4f8bcecc39dc7a9
Sean Taubner <sean.taubner@gmail.com>
2022-09-26 16:53:03 -0500

    add phrases to arrays

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,7 +1,7 @@
 // object containing word and phrase objects
 
 const wordObj = {
-    arr1: [],
-    arr2: [],
-    arr3: [],
+    arr1: ['What has the', 'Hate! Hate! Hate!', 'That is the mark of the Mutant', 'The past', 'If you go looking', 'The gratitude', 'The Emperor\'s'],
+    arr2: ['The Emperor', 'An emotion as pure as it is deep!', 'To be Unclean', 'is always', 'for trouble,', 'of the powerful', 'light'],
+    arr3: ['ever done for me?', 'Let it flow, let it run free!', 'To be Reviled', 'with us.', 'you\'re sure to find it.', 'is a heavy weight to bear.', 'is my torch!']
 }
