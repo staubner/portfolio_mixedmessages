fbc2f83e338397368062a8ada8a6808503f992da
Sean Taubner <sean.taubner@gmail.com>
2022-09-27 15:30:10 -0500

    complete working script, new phrase fragments may be added to arrays without worry

--- mixed_messages.js
+++ mixed_messages.js
@@ -5,3 +5,12 @@ const wordObj = {
     arr2: ['The Emperor', 'An emotion as pure as it is deep!', 'To be Unclean', 'is always', 'for trouble,', 'of the powerful', 'light'],
     arr3: ['ever done for me?', 'Let it flow, let it run free!', 'To be Reviled', 'with us.', 'you\'re sure to find it.', 'is a heavy weight to bear.', 'is my torch!']
 }
+
+const phraseGen = () => {
+    let first = wordObj.arr1[Math.floor(Math.random() * wordObj.arr1.length)];
+    let second = wordObj.arr2[Math.floor(Math.random() * wordObj.arr2.length)];
+    let third = wordObj.arr3[Math.floor(Math.random() * wordObj.arr3.length)];
+    console.log(`${first} ${second} ${third}`);
+}
+
+phraseGen()
